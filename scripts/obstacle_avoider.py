256a94a8a30e2a5afcb9b3bcca9225578815b6c7
Jeff Pflueger <jeffrey.pflueger@students.olin.edu>
2017-02-09 20:12:12 -0500

    refining obstacle avoid

--- scripts/obstacle_avoider.py
+++ scripts/obstacle_avoider.py
@@ -22,13 +22,15 @@ class ObstacleAvoid(object):
 
         #control attribute
         self.scanArea = 60
-        self.kp = 0.3
-        
-	
+        self.kAngular = 0.3
+        self.kLinear = 0.2
+        self.state = 'passive'
+
+
 	#cmd and sub storage attributes
         self.ranges = []
         self.cmd = Twist()
-        self.cmd.linear.x = 0.2
+        self.cmd.linear.x = 0
         self.viz = Marker()
 
         #current status attributes
@@ -36,7 +38,6 @@ class ObstacleAvoid(object):
 
     def processScan(self, msg):
         """updates stored laser scan and calls conversion to force"""
-        print("Scanning") 
         self.ranges = msg.ranges
         self.processForces()
 
@@ -46,44 +47,70 @@ class ObstacleAvoid(object):
         for i in range(len(self.ranges)):
             if i <= self.scanArea or i >= 360 - self.scanArea:
                 if self.ranges[i]:
-                    x = math.cos(math.radians(i))/self.ranges[i] 
+                    x = math.cos(math.radians(i))/self.ranges[i]
                     y = math.sin(math.radians(i))/self.ranges[i]
                     forces.append([x, y])
         if not forces:
-            forces= [0,0]
+            self.netForces = [[0, 0]]
             print "Nothing to See"
+            netR = 0
+            netTheta = 0
+        else:
+            #add forces
+            netX = 0
+            netY = 0
+            for i in forces:
+                netX += i[0]/len(forces)
+                netY += i[1]/len(forces)
+                #print(netX, netY)
 
-        #add forces 
-        netX = 0
-        netY = 0
-        for i in forces:
-            netX += i[0]/len(forces)
-            netY += i[1]/len(forces)
-        print(netX, netY)  
-
-	#convert to polar coordinates for use with angular p-controller
-	netTheta = math.atan(netY/netX)
-        netR = -math.sqrt(netX**2 + netY**2)
-        netTheta = math.atan(netY/netX)
+	               #convert to polar coordinates for use with angular p-control
+            netTheta = math.atan(netY/netX)
+            netR = -math.sqrt(netX**2 + netY**2)
 
         #constraint robot to never retreat to avoid reverse motion collisions
-        if netR > 0.0:
-           netR = 0.0
+            if netR > 0.0:
+                netR = 0.0
 
 	#update net force attribute
-        self.netForce = [netR, netTheta]
-        print("{0:.2f}".format(netR), "{0:.2f}".format(netTheta)) #print 2 decimals of net force 
+            self.netForce = [netR, netTheta]
+        #print("{0:.2f}".format(netR), "{0:.2f}".format(netTheta)) #print 2 decimals of net force
 
-    def setCmd(self):
-        self.cmd.angular.z = self.netForce[0] * self.kp * math.copysign(1,self.netForce[1])
-        print("Turn" + str(self.cmd.angular.z))
- 
+    def setCmdPassive(self):
+        self.cmd.angular.z = self.netForce[0] * self.kAngular * math.copysign(1,self.netForce[1])
+        print('Force: ' + str(self.netForce[0]))
+        if math.fabs(self.netForce[0]) < 0.1:
+            self.cmd.angular.z = 0
+        self.cmd.linear.x = self.netForce[1]*self.kLinear*math.copysign(1, self.netForce[1])
+        #print("Turn" + str(self.cmd.angular.z))
+
+    def setCmdActive(self):
+        self.cmd.angular.z = 0.1
+        self.cmd.linear.x = 0[0]
+
+    def checkState(self):
+        print self.netForce[0]
+        if self.netForce[0] < -1.5:
+            self.state = 'active'
+            print('act')
+        else:
+            self.state = 'passive'
+            print('pass')
+
+        print("State")
+
     def main(self):
         """ Run Loop -- currently just allows CB to run"""
         while not rospy.is_shutdown():
-            self.setCmd()
+            self.checkState()
+            if self.state == 'passive':
+                self.setCmdPassive()
+            if self.state == 'active':
+                self.setCmdActive()
             self.pubCmd.publish(self.cmd)
+            print(self.state)
 	    self.r.sleep()
+
 
 # Run instance
 robit = ObstacleAvoid()
